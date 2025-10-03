67743e5a12c73d98458396663abf4ee7748fe911
TThi <24521654@gm.uit.edu.vn>
2025-10-03 20:46:50 +0700

    Add info form

--- BT3_LTMCB/DK.Designer.cs
+++ BT3_LTMCB/DK.Designer.cs
@@ -122,7 +122,7 @@
             // button1
             // 
             button1.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(317, 332);
+            button1.Location = new Point(322, 330);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 9;
---
+++ BT3_LTMCB/INFOMATION.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace BT3_LTMCB
+{
+    partial class INFOMATION
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(70, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(371, 32);
+            label1.TabIndex = 0;
+            label1.Text = "THÔNG TIN NGƯỜI DÙNG ";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(491, 355);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 44);
+            button1.TabIndex = 2;
+            button1.Text = "Đăng Xuất";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(104, 119);
+            label2.Name = "label2";
+            label2.Size = new Size(80, 17);
+            label2.TabIndex = 3;
+            label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(116, 199);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 17);
+            label3.TabIndex = 4;
+            label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(254, 199);
+            label4.Name = "label4";
+            label4.Size = new Size(216, 20);
+            label4.TabIndex = 5;
+            label4.Text = "\"Email người dùng đã đăng ký\"";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(242, 116);
+            label5.Name = "label5";
+            label5.Size = new Size(202, 20);
+            label5.TabIndex = 6;
+            label5.Text = "\"Tên người dùng đã đăng ký\"";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Times New Roman", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(639, 355);
+            button2.Name = "button2";
+            button2.Size = new Size(115, 44);
+            button2.TabIndex = 7;
+            button2.Text = "Thoát";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // INFOMATION
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "INFOMATION";
+            Text = "INFOMATION";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button2;
+    }
+}
